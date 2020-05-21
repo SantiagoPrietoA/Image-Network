d4fee1eea07d1ce5735d524830773d577ebbb913
Santiago Prieto <santiagoprietoacero@gmail.com>
2020-05-21 15:29:12 -0500

    deploy.7

--- src/database.js
+++ src/database.js
@@ -9,16 +9,3 @@ mongoose
   })
   .then(() => console.log("database connected"))
   .catch(() => console.error("Database error conection"));
-
-// const MongoClient = require("mongodb").MongoClient;
-// const uri =
-//   "mongodb+srv://imagedb:<password>@cluster0-yg9o2.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// client.connect((err) => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
