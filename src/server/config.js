2d0b3955fece5a9bb504789ca2132ed995b5068d
Santiago Prieto <santiagoprietoacero@gmail.com>
2020-05-21 15:00:08 -0500

    deploy.5.1

--- src/server/config.js
+++ src/server/config.js
@@ -11,7 +11,7 @@ const Handlebars = require("handlebars");
 const app = express();
 
 // setings
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3001);
 app.set("views", path.join(__dirname, "../views"));
 app.engine(
   ".hbs",
