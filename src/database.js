d504869ce390233768114192cdb602d99011bc66
Santiago Prieto <santiagoprietoacero@gmail.com>
2020-05-21 15:19:31 -0500

    deploy.6

--- src/database.js
+++ src/database.js
@@ -1,14 +1,24 @@
 const mongoose = require("mongoose");
 
-const URI = process.env.MONGODB_URI
-  ? process.env.MONGODB_URI
-  : "mongodb://localhost/databasetest";
+const URI =
+  "mongodb+srv://imagedb:imagedb@cluster0-yg9o2.mongodb.net/test?retryWrites=true&w=majority";
 
 mongoose
   .connect(URI, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   })
   .then(() => console.log("database connected"))
   .catch(() => console.error("Database error conection"));
+
+// const MongoClient = require("mongodb").MongoClient;
+// const uri =
+//   "mongodb+srv://imagedb:<password>@cluster0-yg9o2.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
