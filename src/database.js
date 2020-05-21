4f13d2da1a7ebee37e848a74b78b1462adbcfe24
Santiago Prieto <santiagoprietoacero@gmail.com>
2020-05-21 14:51:43 -0500

    deploy.4

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-const URI =
-  "mongodb+srv://imagedb:imagedb@cluster0-yg9o2.mongodb.net/test?retryWrites=true&w=majority";
+const URI = process.env.MONGODB_URI
+  ? process.env.MONGODB_URI
+  : "mongodb://localhost/databasetest";
 
 mongoose
   .connect(URI, {
